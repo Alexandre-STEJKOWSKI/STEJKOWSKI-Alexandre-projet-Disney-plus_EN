17d68b282dfc144b630f66271c3a401101c00401
Alexandre <150161151+Alexandre-STEJKOWSKI@users.noreply.github.com>
2024-10-04 17:21:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 // script.js
-document.querySelectorAll('.faq-item').forEach(item => {
+document.querySelectorAll('.article-FAQ').forEach(item => {
     item.addEventListener('click', () => {
         // Si l'élément est déjà actif, on le désactive
         if (item.classList.contains('active')) {
             item.classList.remove('active');
-            item.querySelector('.faq-toggle').textContent = '+';
+            item.querySelector('.FAQ-basculer').textContent = '+';
             return;
         }
         // Fermer tous les autres éléments FAQ
-        document.querySelectorAll('.faq-item').forEach(i => {
+        document.querySelectorAll('.article-FAQ').forEach(i => {
             i.classList.remove('active');
-            i.querySelector('.faq-toggle').textContent = '+';
+            i.querySelector('.FAQ-basculer').textContent = '+';
         });
         // Activer l'élément courant
         item.classList.add('active');
-        item.querySelector('.faq-toggle').textContent = '-' ;
+        item.querySelector('.FAQ-basculer').textContent = '-' ;
     });
 });
